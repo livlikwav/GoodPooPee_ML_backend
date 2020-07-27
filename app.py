672154c3ce2dc1eb4fc7c368f2d09b2a9ef53055
Gyeongmin Ha <gaonrudal@gmail.com>
2020-07-28 03:28:43 +0900

    add main tables

--- app.py
+++ app.py
@@ -23,6 +23,46 @@ class User(db.Model):
     created_date = db.Column(db.DateTime, nullable = False)
     last_modified_date = db.Column(db.DateTime, nullable = False)
 
+    def __repr__(self):
+        return f"<User : {self.id}, {self.email}, {self.first_name}, {self.last_name}>"
+
+class Pet(db.Model):
+    __tablename__ = 'Pet'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(250), nullable = False)
+    breed = db.Column(db.String(250), nullable = False)
+    gender = db.Column(db.String(250), nullable = False)
+    birth = db.Column(db.DateTime, nullable = False)
+    adoption = db.Column(db.DateTime, nullable = False)
+    created_date = db.Column(db.DateTime, nullable = False)
+    last_modified_date = db.Column(db.DateTime, nullable = False)
+
+    def __repr__(self):
+        return f"<Pet : {self.id}, {self.name}, {self.breed}, {self.gender}, {self.birth}, {self.adoption}>"
+
+class Ppcam(db.Model):
+    __tablename__ = 'Ppcam'
+    id = db.Column(db.Integer, primary_key = True)
+    serial_num = db.Column(db.String(250), nullable = False)
+    created_date = db.Column(db.DateTime, nullable = False)
+    last_modified_date = db.Column(db.DateTime, nullable = False)
+
+    def __repr__(self):
+        return f"<Ppcam : {self.id}, {self.serial_num}>"
+
+class Pad(db.Model):
+    __tablename__ = 'Pad'
+    id = db.Column(db.Integer, primary_key = True)
+    lu = db.Column(db.Integer, nullable = False)
+    ld = db.Column(db.Integer, nullable = False)
+    ru = db.Column(db.Integer, nullable = False)
+    rd = db.Column(db.Integer, nullable = False)
+    created_date = db.Column(db.DateTime, nullable = False)
+    last_modified_date = db.Column(db.DateTime, nullable = False)
+
+    def __repr__(self):
+        return f"<Pad : {self.id}, {self.iu}, {self.id}, {self.ru}, {self.rd}>"
+
 db.create_all()
 
 @app.route('/', methods=['GET'])
