5ed2554af6993ff6d06a6a07a171786c2846af98
Gyeongmin Ha <gaonrudal@gmail.com>
2020-10-13 04:48:59 +0900

    Add confirm device decorator and apply to POST pet record

--- app/resources/pet_record.py
+++ app/resources/pet_record.py
@@ -4,7 +4,7 @@ from flask import request, jsonify
 from flask_restful import Resource
 from app.models.pet import Pet
 from app.models.pet_record import PetRecord
-from app.utils.decorators import confirm_account
+from app.utils.decorators import confirm_account, confirm_device
 from app import db, ma
 import datetime
 
@@ -25,6 +25,7 @@ deleted_record_schema = PetRecordSchema(only=("timestamp", "pet_id", "user_id"))
 record_query_schema = RecordQuerySchema()
 
 class PetRecordApi(Resource):
+    @confirm_device
     def post(self, pet_id):
         """
         Post new pet record by Ppcam(device)
--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -1,4 +1,5 @@
-from flask import request, jsonify
+from app.models.ppcam import Ppcam
+from flask import request
 from app.models.user import User
 import functools
 
@@ -23,14 +24,48 @@ def confirm_account(func):
                 return func(*args, **kwargs)
             # Invalid token(get string error msg)
             else: 
-                return jsonify({
+                return {
                     'status' : 'Fail',
                     'message' : resp
-                }), 401
+                }, 401
         else:
-            return jsonify({
+            return {
                 'status' : 'Fail',
                 'message' : 'Request provide a invalid auth token.'
-            }), 401
+            }, 401
+
+    return decorator
+
+
+def confirm_device(func):
+    """
+    Confirm device JWT
+    
+    :Return: func if vaild token, else json string(fail msg)
+    """
+    @functools.wraps(func)
+    def decorator(*args, **kwargs):
+        # check JWT
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = Ppcam.decode_auth_token(auth_token)
+            # Vaild token(get ppcam_id Integer)
+            if not isinstance(resp, str):
+                return func(*args, **kwargs)
+            # Invalid token(get string error msg)
+            else: 
+                return {
+                    'status' : 'Fail',
+                    'message' : resp
+                }, 401
+        else:
+            return {
+                'status' : 'Fail',
+                'message' : 'Request provide a invalid auth token.'
+            }, 401
 
     return decorator
