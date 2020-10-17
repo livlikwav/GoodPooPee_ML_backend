c1b2e82d226f6277db9b96ba886ba7592094883e
Gyeongmin Ha <gaonrudal@gmail.com>
2020-10-18 04:32:08 +0900

    Add redis container and improve docker-compose yml

---
+++ task.py
@@ -0,0 +1,74 @@
+# full task sample
+{
+    'ppsnack' : {
+        'id' : 1,
+        'user_id' : 1,
+        'serial_num' : 'ABCED12314ASDQ',
+        'feedback' : 1.0,
+        'created_date' : 'yyyy-MM-dd HHMMSS',
+        'last_modified_date' : 'yyyy-MM-dd HHMMSS'
+    },
+    'pad' : {
+        'ldx' :  10,
+        'ldy' :  0,
+        'lux' :  0,
+        'luy' :  0,
+        'rdx' :  0,
+        'rdy' :  0,
+        'rux' :  0,
+        'ruy' :  0
+    }
+}
+# empty sample
+{}
+
+# ============== legacy 1 
+# # full task sample
+# {
+#     'tasks': [
+#         {
+#             'method_type' : 'PUT',
+#             'resource' : 'pad',
+#             'data' : {
+#                 'ldx' :  10,
+#                 'ldy' :  0,
+#                 'lux' :  0,
+#                 'luy' :  0,
+#                 'rdx' :  0,
+#                 'rdy' :  0,
+#                 'rux' :  0,
+#                 'ruy' :  0
+#             }
+#         },
+#         {
+#             'method_type' : 'POST',
+#             'resource' : 'snackbar',
+#             'data' : {
+#                 'id' : 1,
+#                 'user_id' : 1,
+#                 'serial_num' : 'ABCED12314ASDQ',
+#                 'feedback' : 1.0,
+#                 'created_date' : 'yyyy-MM-dd HHMMSS',
+#                 'last_modified_date' : 'yyyy-MM-dd HHMMSS'
+#             }
+#         },
+#         {
+#             'method_type' : 'PUT',
+#             'resource' : 'snackbar',
+#             'data' : {
+#                 'id' : 1,
+#                 'user_id' : 1,
+#                 'serial_num' : 'ABCED12314ASDQ',
+#                 'feedback' : 0.75,
+#                 'created_date' : 'yyyy-MM-dd HHMMSS',
+#                 'last_modified_date' : 'yyyy-MM-dd HHMMSS'
+#             }
+#         }
+#     ]
+# }
+
+# # empty task sample
+# {
+#     'tasks': [
+#     ]
+# }
